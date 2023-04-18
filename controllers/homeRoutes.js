d4629d97d4b5c339f1bb099d25131fa74f4beb7a
bcebel <bcebel@gmail.com>
2023-04-17 20:00:09 -0400

    homepage basic render

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,81 @@
 const router = require("express").Router();
-const { Project, User } = require("../models");
+const { Restaurant, User } = require("../models");
 const withAuth = require("../utils/auth");
 
+// GET all galleries for homepage
+router.get("/", async (req, res) => {
+  try {
+    const restaurantData = await Restaurant.findAll();
+
+    const restaurants = restaurantData.map((restaurant) =>
+      restaurant.get({ plain: true })
+    );
+
+    res.render("homepage", {
+      restaurants,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one gallery
+// Use the custom middleware before allowing the user to access the gallery
+router.get("/gallery/:id", withAuth, async (req, res) => {
+  try {
+    const dbGalleryData = await Gallery.findByPk(req.params.id, {
+      include: [
+        {
+          model: Painting,
+          attributes: [
+            "id",
+            "title",
+            "artist",
+            "exhibition_date",
+            "filename",
+            "description",
+          ],
+        },
+      ],
+    });
+
+    const gallery = dbGalleryData.get({ plain: true });
+    res.render("gallery", { gallery, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one painting
+// Use the custom middleware before allowing the user to access the painting
+router.get("/painting/:id", withAuth, async (req, res) => {
+  try {
+    const dbPaintingData = await Painting.findByPk(req.params.id);
+
+    const painting = dbPaintingData.get({ plain: true });
+
+    res.render("painting", { painting, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
+
+/*
 router.get("/", async (req, res) => {
   return res.render("login");
   try {
@@ -85,3 +159,4 @@ router.get("/login", (req, res) => {
 });
 
 module.exports = router;
+*/
