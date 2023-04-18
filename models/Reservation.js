d52004fc92e09ed0416aadadb56dc7b329fdc50f
Joey Deshaies <117233121+jdeshaies@users.noreply.github.com>
2023-04-18 17:03:57 -0400

    Revert "heroku deployment"

---
+++ models/Reservation.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Reservation extends Model {}
+
+Reservation.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    party_number: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    time: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    restaurant_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "restaurant",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "reservation",
+  }
+);
+
+module.exports = Reservation;
