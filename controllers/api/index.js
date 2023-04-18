13d05505257a6b5491630c8cf39e0dd8600bce27
bcebel <bcebel@gmail.com>
2023-04-17 22:41:56 -0400

    added post route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,8 @@ const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const projectRoutes = require("./projectRoutes");
 const { signup, gEmail } = require("../email");
-
+const reservationRoutes = require("./reservationRoutes");
+router.use("/reservation", reservationRoutes);
 router.use("/users", userRoutes);
 router.post("/email", gEmail);
 
