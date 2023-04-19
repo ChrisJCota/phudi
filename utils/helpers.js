17cfff46f227e127aa8fd286d7109e40127a79c2
jd479800 <jd479800@gmail.com>
2023-04-19 19:13:15 -0400

    Formats military time to 12 hour time

--- utils/helpers.js
+++ utils/helpers.js
@@ -7,6 +7,12 @@ module.exports = {
     // Format date as MM/DD/YYYY
     return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit"});
   },
+  format_military_time: (date) => {
+    if (date > 12) {
+      const newDate = date - 12;
+      return newDate;
+    }
+  },
   format_amount: (amount) => {
     // format large numbers with commas
     return parseInt(amount).toLocaleString();
