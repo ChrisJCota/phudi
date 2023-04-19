cce7f23b5362545f64eb45680e6f05f69d826cea
jd479800 <jd479800@gmail.com>
2023-04-18 20:55:09 -0400

    Formats the date/time of the resercvation on profile page

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,6 +3,10 @@ module.exports = {
     // Format date as MM/DD/YYYY
     return date.toLocaleDateString();
   },
+  format_time: (date) => {
+    // Format date as MM/DD/YYYY
+    return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit"});
+  },
   format_amount: (amount) => {
     // format large numbers with commas
     return parseInt(amount).toLocaleString();
